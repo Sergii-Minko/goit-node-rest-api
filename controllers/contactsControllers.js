43aabd871cbd2cdf89fa86f187de47f020b2cbba
Sergii Minko <minkosergii@gmail.com>
2024-06-29 22:59:29 +0200

    update

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,72 @@
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+const { HttpError, ctrlWrapper, validateBody } = require("../helpers");
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.getContactById(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.removeContact(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  // res.status(204).send()
+  res.json({
+    message: "Delete success",
+  });
+};
 
-export const updateContact = (req, res) => {};
+export const createContact = async (req, res) => {
+  const result = await contactsService.addContact(req.body);
+  res.status(201).json(result);
+};
+
+export const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.updateById(id, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const updateById = async (req, res) => {
+  const { id } = req.params;
+  const result = await books.updateById(id, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const deleteById = async (req, res) => {
+  const { id } = req.params;
+  const result = await books.deleteById(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  // res.status(204).send()
+  res.json({
+    message: "Delete success",
+  });
+};
+
+exports.contactsControllers = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
+};
---
+++ helpers/index.js
@@ -0,0 +1,3 @@
+export const HttpError = require("./HttpError");
+export const ctrlWrapper = require("./ctrlWrapper");
+export const validateBody = require("./validateBody");
