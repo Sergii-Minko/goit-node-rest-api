51151116469de79344ddf05a293e84fe08321095
Sergii Minko <minkosergii@gmail.com>
2024-07-07 14:53:41 +0200

    deploy

---
+++ index.js
@@ -0,0 +1,9 @@
+import initMongoDBConnection from "./db/initMongoDBConnection.js";
+import startServer from "./server.js";
+
+const bootstrap = async () => {
+  await initMongoDBConnection();
+  startServer();
+};
+
+bootstrap();
