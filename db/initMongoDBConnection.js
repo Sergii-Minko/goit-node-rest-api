fec5b6b022414e218625b21de8c4d4911bef5f4b
Sergii Minko <minkosergii@gmail.com>
2024-07-07 16:55:35 +0200

    edit env

--- db/initMongoDBConnection.js
+++ db/initMongoDBConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-// import env from '../utils/env.js';
+//  import env from '../utils/env.js';
 
 const initMongoDBConnection = async () => {
   try {
@@ -9,7 +9,7 @@ const initMongoDBConnection = async () => {
     // const user = env('MONGODB_USER');
     // const password = env('MONGODB_PASSWORD');
     // const url = env('MONGODB_URL');
-    // const name = env('MONGODB_NAME');
+    // const DB_HOST = env("DB_HOST");
     // const DB_HOST = `mongodb+srv://${user}:${password}@${url}/${name}?retryWrites=true&w=majority&appName=ClusterGoit
     // const DB_HOST = `mongodb+srv://Goit:hFhyq0PUyppEjt5i@clustergoit.m9yciu2.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=ClusterGoit`;
     await mongoose.connect(DB_HOST);
